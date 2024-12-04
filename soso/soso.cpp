0b9b17ac0bcc09aeeaa2ef09fd3191b99ff66eef
ZelenchatoeSolnyshko <sonyacat371@gmail.com>
2024-12-04 22:36:01 +0300

    mem

--- soso/soso.cpp
+++ soso/soso.cpp
@@ -21,38 +21,3 @@ int main()
 //   4. В окне "Список ошибок" можно просматривать ошибки.
 //   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
 //   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
-/*int n;
-    cout << "Enter the size of vectors for first function: ";
-    cin >> n;
-
-    vector<double> x1(n);
-    vector<double> y1(n);
-    vectors(n, x1);
-    vectors(n, y1);
-
-    double value1;
-    cout << "Enter the value for the interpolation for first function: ";
-    cin >> value1;
-
-    double itog = lagrangeInterpolation(n, x1, y1, value1);
-
-    cout << "This is the Value  of interpotion of Lagrange for first function " << value1 << itog << endl;
-
-    //вторая функция
-    int m;
-    cout << "Enter the size of vectors for second function: ";
-    cin >> m;
-
-    vector<double> x2(m);
-    vector<double> y2(m);
-    vectors(m, x2);
-    for (int i = 0; i < m; i++) 
-    {
-        y2[i] = pow(x2[i], 3) + 2 * x2[i];
-    }
-    double value2;
-    cout << "Enter the value for the interpolation for second function: ";
-    cin >> value2;
-
-    double res = lagrangeInterpolation(m, x2, y2, value2);
-    cout << "This is the Value  of interpotion of Lagrange for second function " << value2 << res << endl;*/
